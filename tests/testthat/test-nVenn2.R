468183ebb01ebdf329ca2887118276514c3ceb80
vqf <quesadavictor@uniovi.es>
2026-07-15 12:13:35 +0200

    Improved tests

--- tests/testthat/test-nVenn2.R
+++ tests/testthat/test-nVenn2.R
@@ -1,5 +1,5 @@
 test_that("nVenn object has the necessary fields", {
-  testObject <- nVennDiagram(exampledf, plot = F, verbose = F)
+  testObject <- nVennDiagram(exampledf, plot = FALSE, verbose = FALSE)
   
   expect_contains(names(testObject), 
                   c('desc', 'setNames', 'opts'))
@@ -9,20 +9,20 @@ test_that("nVenn object has the necessary fields", {
 })
 
 test_that("Object can be modified", {
-  testObject <- nVennDiagram(exampledf, plot = F, verbose = F)
+  testObject <- nVennDiagram(exampledf, plot = FALSE, verbose = FALSE)
   colorVector <- c("red", "grey")
   colorList <- list(SAS="blue", PYTHON="#00ff11")
   mytheme <- list(opacity=0.2, lineWidth=2, fontSize=16, 
                   showRegions=F, 
                   colors=c("red", "green", "blue", "black", "#ffff00"))
   
-  expect_no_warning(testObject <- setVennColor(testObject, "SAS", 'black'))
-  expect_no_warning(testObject <- setVennPalette(testObject, palette = 2))
-  expect_no_warning(testObject <- setVennOpts(testObject, palette = 2))
-  expect_no_warning(testObject <- setVennColors(testObject, colorVector))
-  expect_no_warning(testObject <- setVennColors(testObject, colorList))
-  expect_no_warning(testObject <- setVennSkin(testObject, mytheme))
-  expect_warning(testObject <- setVennColor(testObject, "Unexisting", 'black'))
+  expect_no_warning(testObject <- setVennColor(testObject, "SAS", 'black', plot = FALSE))
+  expect_no_warning(testObject <- setVennPalette(testObject, palette = 2, plot = FALSE))
+  expect_no_warning(testObject <- setVennOpts(testObject, palette = 2, plot = FALSE))
+  expect_no_warning(testObject <- setVennColors(testObject, colorVector, plot = FALSE))
+  expect_no_warning(testObject <- setVennColors(testObject, colorList, plot = FALSE))
+  expect_no_warning(testObject <- setVennSkin(testObject, mytheme, plot = FALSE))
+  expect_warning(testObject <- setVennColor(testObject, "Unexisting", 'black', plot = FALSE))
 })
 
 
